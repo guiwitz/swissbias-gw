4b80eec96df4dcc2e03cd0aeb80eb5659c1aea19
guiwitz <guiwitz@users.noreply.github.com>
2024-06-20 16:44:51 +0200

    add test

--- src/swissbias_gw/algos.py
+++ src/swissbias_gw/algos.py
@@ -7,21 +7,3 @@ def add_one(x):
 
 def add_two(x):
     return np.array(x) + 2
-
-
-def newfun(x, b):
-    newitem = [
-        "name1",
-        "name2",
-        "name3",
-        "name4",
-        "name5",
-        "name6",
-        "name7",
-        "name8",
-        "name9",
-        "name10",
-        "name11",
-        "name12",
-    ]
-    return np.array(x) + 4, newitem
--- tests/test_swissbias_gw.py
+++ tests/test_swissbias_gw.py
@@ -1,2 +1,6 @@
-def test_something():
-    pass
+from swissbias_gw.algos import add_one
+
+
+def test_add_one():
+    out = add_one(1)
+    assert out == 2, f"Expected 2, but got {out}"
